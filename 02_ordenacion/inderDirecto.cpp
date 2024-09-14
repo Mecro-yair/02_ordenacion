5c8881aaa1c575645cf0b62831412a0b4a644564
Yair292 <dluquep@unjbg.edu.pe>
2024-09-13 20:58:53 -0500

    Agregar Intercambio Directo Izquierda

--- 02_ordenacion/inderDirecto.cpp
+++ 02_ordenacion/inderDirecto.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
+
 void InterDirectoDer(int a[],int n);
 void mostrarFuncion(int arreglo[],int tamanio);
+void InterDirectoIzq(int a[],int n);
+
 int main(){
 	int n;
 	int op;
-	cout << "Digite la capacidad de arreglo:"<<endl
-	; cin >> n;
+	cout << "Digite la capacidad de arreglo:"<<endl; 
+	cin >> n;
 	int arreglo[n];
 	int aux[n];
 	
@@ -18,29 +21,32 @@ int main(){
 	for(int i=0 ; i<n;i++){
 		aux[i] = arreglo[i];
 	}
-	//menu
-	do{
-		cout << "Menu de opciones:\n"
-			<<"1. Intercambio directo por la derecha .\n"
-			<<"2. Intercambio directo izquierdo.\n"
-			<<"3. Intercambio directo con señal.\n"
-			<<"4. Intercambio directo bidireccional.\n"
-			<<"0. Salir.\n";
-		cin >> op;
-		switch (op){
-			case 1:
-				InterDirectoDer(arreglo,n-1);
-				mostrarFuncion(arreglo,n);
-					
-		}
-	}while(op !=0);
+	cout << "Menu de opciones:\n"
+		<<"1. Intercambio directo por la derecha .\n"
+		<<"2. Intercambio directo izquierdo.\n"
+		<<"3. Intercambio directo con señal.\n"
+		<<"4. Intercambio directo bidireccional.\n"
+		<<"0. Salir.\n";
+	cin >> op;
+	switch (op){
+		case 1:
+			InterDirectoDer(arreglo,n);
+			mostrarFuncion(arreglo,n);
+			break;
+		case 2:
+			InterDirectoIzq(arreglo,n);
+			mostrarFuncion(arreglo,n);
+			break;
+				
+	}
 	
 	return 0;
 }
 void InterDirectoDer(int a[],int n){
+	n--;
 	int aux = 0;
-	for(int i =0;i<n-1;i++){
-		for(int j = 0; j<n-i;j++){
+	for(int i=0;i<n-1;i++){
+		for(int j=0; j<n-i;j++){
 			if (a[j] > a[j+1]){
 				aux = a[j];
 				a[j]= a[j+1];
@@ -49,6 +55,22 @@ void InterDirectoDer(int a[],int n){
 		}
 	}
 }
+void InterDirectoIzq(int a[],int n) {
+	n--;
+	int aux=0;
+	for (int i=1 ;i<=n ;i++) {
+		for (int j=n ;j >= i; j--) {         
+			if (a[j]<a[j-1]) {
+				aux=a[j-1];
+				a[j-1]=a[j];
+				a[j]=aux;
+			}
+		}
+	}
+}
+
+
+
 void mostrarFuncion(int arreglo[],int tamanio){
 	for(int i =0 ; i<tamanio; i++){
 		cout << arreglo[i]<<" ";
